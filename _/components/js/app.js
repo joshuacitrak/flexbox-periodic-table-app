8e2e117ab65d9a38da8013f0527db6227eb1cda9
joshuacitrak <joshua.citrak@apollo.edu>
2015-04-22 15:14:40 -0700

    added assets, layout, json file

--- _/components/js/app.js
+++ _/components/js/app.js
@@ -1,1 +1,37 @@
+(function() {
+	var app = angular.module('PTApp', ['ngTouch' ]).config(
+			router);
+    
+    var dataObj = [];
+
+	app.controller('PageController', function($scope, $rootScope, $http) {
+        //move this to service
+		$http.get('_/js/data.json').success(
+				function(data, status, headers, config) {
+                    
+					$scope.dataObj = data;
+                    
+				}).error(function(data, status, headers, config) {
+			alert('data could not be loaded. epic fail.');
+		});//error
+	});
+
+});
 //https://github.com/diniska/chemistry/blob/master/PeriodicalTable/periodicTable.json 
+/**
+app.factory('jsondata', function($http) { 
+
+    var obj = {content:null};
+
+    $http.get('_/js/data.json').success(
+				function(data, status, headers, config) {
+        // you can do some processing here
+        obj.content = data;
+    }).error(function(data, status, headers, config) {
+			alert('data could not be loaded. epic fail.');
+		});//error 
+
+    return obj;    
+});
+
+**/
